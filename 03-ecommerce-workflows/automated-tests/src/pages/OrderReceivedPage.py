8735f05051564f0438b68080a290cdaf149a0fcf
NicoleQA <190654771+shontelleQA@users.noreply.github.com>
2025-10-27 21:06:57 -0400

    feat(pages): add order received page verification method

--- 03-ecommerce-workflows/automated-tests/src/pages/OrderReceivedPage.py
+++ 03-ecommerce-workflows/automated-tests/src/pages/OrderReceivedPage.py
@@ -5,11 +5,15 @@
 from src.helpers.selenium_extended import SeleniumExtended
 from src.pages.locators.OrderReceivedLocators import OrderReceivedLocators
 
-class OrderReceivedPage:
+
+class OrderReceivedPage(OrderReceivedLocators):
     def __init__(self, driver):
         self.driver = driver
         self.sl = SeleniumExtended(driver)
 
-    # to be added in Part 5:
-    # - wait_for_order_received()
-    # - get_order_number()
+    def verify_order_received_page_loaded(self):
+        """Wait until the 'Order received' header is visible and contains correct text."""
+        expected_text = "Order received"
+        self.sl.wait_until_element_contains_text(
+            self.ORDER_RECEIVED_HEADER, expected_text
+        )
